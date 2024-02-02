9d926015e490fefbfecfdc3b7f8541b557f166e3
VladIhnatovv <149872627+VladIhnatovv@users.noreply.github.com>
2024-02-02 22:52:42 +0200

    Second
    Scnd

--- js/main.js
+++ js/main.js
@@ -58,3 +58,22 @@ footerLinks.forEach((link) => {
     }
   });
 });
+
+/////////////////////
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu__list");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".menu__link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
